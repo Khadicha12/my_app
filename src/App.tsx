e6599375feac6ba3543ea5a726af22f7c2507ca4
Khadicha12 <khadicha.madbobaeva@mail.ru>
2026-05-23 14:23:38 +0500

    youtobe project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import './App.css'
 
 function App() {
-  <div>
-    <h1>khadicha abdulayeva</h1>
-    <p>something</p>
-    <h1>nothing</h1>
-    <p>vshcghdscgshxsx</p>
-  </div>
-}
 
+}
 export default App
